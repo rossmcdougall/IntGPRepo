79d322ed614a4535f98859bf4b4d5b5951df4921
MCDOUGALL <rmcdou202@lab-m627-osx-08.local>
2014-10-03 14:12:39 +0100

    w
    w

--- UnityGitProject/Assets/TestScript.cs
+++ UnityGitProject/Assets/TestScript.cs
@@ -1,11 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class TestScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+	     Print(“Hello World”);
 	}
 	
 	// Update is called once per frame
